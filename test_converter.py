a77835d519b26b1ae9cd7784360e7551748d7e3f
indreven <indrevenckauskait@gmail.com>
2025-05-02 20:58:53 +0300

    Update test_converter.py
    3 versija be print

--- test_converter.py
+++ test_converter.py
@@ -7,13 +7,11 @@ class TestSimple(unittest.TestCase):
     def test_decimal_to_roman(self):
         converter = DecimalToRoman()
         result = converter.convert_to(11)
-        print("\nDecimal 11 =>", result)
         self.assertEqual(result, 'XI')
 
     def test_roman_to_decimal(self):
         converter = RomanToDecimal()
         result = converter.convert_to('XIV')
-        print("\nRoman 'XIV' =>", result)
         self.assertEqual(result, 14)
 
 
